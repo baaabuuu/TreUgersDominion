3df9fee6088f9a49b722ffcdf511d6dbe32d26a6
baaabuuu <baaabuuu@users.noreply.github.com>
2018-01-06 12:43:20 +0100

    Feature Base
    Initial Commits

--- src/testing/PlayerTest.java
+++ src/testing/PlayerTest.java
@@ -9,6 +9,14 @@ import engine.Player;
 
 public class PlayerTest
 {	
+	@Test
+	public void drawCardNoDeck()
+	{
+		Player player = new Player();
+		assertEquals("No cards drawn - hand size is 0", 0, player.getHandSize());
+		player.drawCard(1);
+		assertEquals("No cards drawn - deck is empty - hand size is 0", 0, player.getHandSize());
+	}
 	
 	@Test
 	public void drawACard()
@@ -19,20 +27,21 @@ public class PlayerTest
 		player.drawCard(1);
 		assertEquals("1 cards drawn - hand size is 1", 1, player.getHandSize());
 		player.drawCard(1);
-		assertEquals("1 cards drawn - hand size is 1", 1, player.getHandSize());
+		assertEquals("no cards drawn - hand size is still 1", 1, player.getHandSize());
 	}
 	
 	@Test
-	public void draw2Cards()
+	public void drawNCards()
 	{
+		int n = 2;
 		Player player = new Player();
 		player.addCardDeckBottom(new Card());
 		player.addCardDeckBottom(new Card());
 		assertEquals("No cards drawn - hand size is 0", 0, player.getHandSize());
-		player.drawCard(2);
-		assertEquals("2 cards drawn - hand size is 2", 2, player.getHandSize());
-		player.drawCard(2);
-		assertEquals("2 cards drawn - hand size is 2", 2, player.getHandSize());
+		player.drawCard(n);
+		assertEquals("n cards drawn - hand size is n", n, player.getHandSize());
+		player.drawCard(n);
+		assertEquals("no cards drawn - hand size is still n", n, player.getHandSize());
 	}
 	
 	@Test
@@ -48,7 +57,7 @@ public class PlayerTest
 		assertEquals("2 cards drawn - hand size is 2 - reshuffled discard pile", 2, player.getHandSize());
 		assertEquals("0 cards in discard pile - reshuffled discard pile", 0, player.getDiscard().size());
 		player.drawCard(1);
-		assertEquals("2 cards drawn - hand size is 2 - no more cards can be drawn", 2, player.getHandSize());
+		assertEquals("no cards drawn - hand size is 2 - no more cards can be drawn", 2, player.getHandSize());
 	}
 
 }
