19cbe8c0c541177b9ad7c82d4da75356bfaab4a3
baaabuuu <baaabuuu@users.noreply.github.com>
2018-01-11 12:11:53 +0100

    AI initial

---
+++ src/ai/BigMoney.java
@@ -0,0 +1,13 @@
+package ai;
+
+import Objects.BoardState;
+
+public class BigMoney implements AI {
+
+	BoardState state;	
+	
+	public BigMoney() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
