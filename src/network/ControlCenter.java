9e3dadd461d0ca8f1dc8387a9daa8354e2e9257b
Adam <Adam@vlan627.mekanik.router.net.dtu.dk>
2018-01-12 13:47:41 +0100

    Minor changes again

--- src/network/ControlCenter.java
+++ src/network/ControlCenter.java
@@ -3,6 +3,8 @@ package network;
 import org.jspace.ActualField;
 import org.jspace.Space;
 
+import cards.Card;
+
 public class ControlCenter extends Thread {
 	
 	private Space clientSpace, safeSpace;
@@ -18,19 +20,36 @@ public class ControlCenter extends Thread {
 		
 		String name;
 		ClientCommands cmd;
+		Card card;
+		int index;
 		
 		while(true){
 			
 			try {
-				clientSpace.get(new ActualField(name), new ActualField(cmd) );
+				clientSpace.get(new ActualField(name), new ActualField(cmd));
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			switch(cmd){
-			
+				buyCard:
+					clientSpace.get(new ActualField(card));
+					
+					safeSpace.put(name, cmd);
+					safeSpace.put(card);
+				playCard:
+					clientSpace.get(new ActualField(index));
+				
+					safeSpace.put(name, cmd);
+					safeSpace.put(index);
+				selectCard:
+					clientSpace.get(new ActualField(index));
+				
+					safepace.put(name, cmd);
+					safeSpace.put(index);	
 				default:
+					safeSpace.put(name, cmd);
 			}
 			
 		}
--- src/network/Lobby.java
+++ src/network/Lobby.java
@@ -1,81 +1,101 @@
 package network;
 
-
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.jspace.ActualField;
 import org.jspace.SequentialSpace;
 import org.jspace.Space;
 import org.jspace.SpaceRepository;
+import org.jspace.Tuple;
 
 import cards.CardReader;
 import log.Log;
+import engine.Game;
+import engine.GameStarter;
 
 
-public class Lobby {
-	//Setup
-	private static Initializer[] gamesRunning = new Initializer[1000];
-	private static int indexID;
+public class Lobby extends Thread  {
 	
-	//Setup the uri
-	private static int port = 8181;
-	private static String host = "localhost";
-	private static String uri = "tcp://"+ host + ":" + port + "/?keep";
+	private SpaceRepository repository;
+	private String uri;
+	private Space clientSpace, safeSpace;
+	private int noOfPlayers;
+	private int activePlayers;
+	private ArrayList<String> expansions;
+	private CardReader cardReader;
+	private ArrayList<String> players;
+	private Writer writer;
+	private ControlCenter controlCenter;
 	
-	public static void lobby(CardReader cardReader) throws InterruptedException{
+	public Lobby(String uri, CardReader cardReader){
 		
-		
-		indexID = 0;
-		//Setup the Repository
-		SpaceRepository repository = new SpaceRepository();
+		//Setup client space
+		this.uri = uri;
+		repository = new SpaceRepository();
 		repository.addGate(uri);
+		clientSpace = new SequentialSpace();
+		repository.add("game", clientSpace);
 		
-		//Setup the lobby
-		Space lobby = new SequentialSpace();
-		repository.add("lobby", lobby);
+		//Setup safe space
+		safeSpace = new SequentialSpace();
 		
-		//Setting up value holders
-		String name = "";
-		String cmd 	= "";
-		String tempURI = "";
-		int gameID = 0;
 		
+		//Setup control center
+		//TODO
 		
 		
 		
-		//Wait for command
-		while(true){
-			//Reads command
-			lobby.get(new ActualField("server"),new ActualField(cmd));
-			
-			//Obeys command
-			switch(cmd.toLowerCase()){
-			case "enter":		
-				lobby.get(new ActualField("server"), new ActualField(name), new ActualField(gameID));
-				
-				
-				if(indexID < gameID){
-					lobby.put("roomURI", name, gameID, gamesRunning[gameID].getURI());
-				} else {
-					lobby.put(new ActualField("roomURI"),new ActualField(name),new ActualField(gameID),new ActualField("gameNotFoundException"));
-				}
-			case "create":
-				if(indexID < 1000){
-					tempURI = "tcp://"+ host + ":" + port + "/" + indexID +"?keep";
-					Initializer tempInit = new Initializer(tempURI, cardReader);
-					gamesRunning[indexID] = tempInit;
-					lobby.put(new ActualField("roomURI"),new ActualField(name),new ActualField(indexID),new ActualField(tempURI));
-					indexID++;
-				} else {
-					lobby.put(new ActualField("roomURI"),new ActualField(name),new ActualField(1000),new ActualField("toManyGamesException"));
-				}
-			default:
-				
-			}
+		this.noOfPlayers = 4;
+		this.activePlayers = 0;
+	
+		this.cardReader = cardReader;
+		
+		expansions.add("base");
+		
+		
 		}
 	
-	
-	}
- 
+	public void run(){
 		
+		
+		String name = "";
+		Log.log("Searching for players");
+		while(activePlayers < noOfPlayers){
+			try {
+				clientSpace.get(new ActualField(name), new ActualField("enter") );
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Log.log("Player found. Name of user: " + name);
+			
+			players.add(name);
+			
+			
+		}
+		
+
+		//Setup writer
+		writer = new Writer(clientSpace, players.toArray(new String[noOfPlayers]));
+		
+		//Setup controlcenter
+		controlCenter = new ControlCenter(clientSpace, safeSpace);
+		controlCenter.start();
+		
+		//Setup game		
+		GameStarter gameStarter = new GameStarter(noOfPlayers, players.toArray(new String[noOfPlayers]), cardReader, expansions, new Random(), writer, safeSpace);
+		gameStarter.startGame();
+		Log.log("Game start");
+		
+	}
+	
+	public String getURI(){
+		return uri;
+	}
+	
+	
 }
+
+
