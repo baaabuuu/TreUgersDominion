49d3c060755b4d6c1236c6932a316ff02b4f7cdb
Adammat <31769674+Adammat@users.noreply.github.com>
2018-01-18 18:11:35 +0100

    ArrayListObject made

---
+++ src/objects/ArrayListObject.java
@@ -0,0 +1,25 @@
+package objects;
+
+import java.util.ArrayList;
+
+public class ArrayListObject {
+	private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+	
+	public ArrayListObject(ArrayList<Integer> arrayList){
+		this.arrayList = arrayList;
+	}
+	
+	
+	
+	public ArrayList<Integer> getArrayList() {
+		return arrayList;
+	}
+
+	public void setArrayList(ArrayList<Integer> arrayList) {
+		this.arrayList = arrayList;
+	}
+
+	
+	
+	
+}
